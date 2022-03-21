051114d10ffb7abee254c61349308caf86b2c88d
ManasMalla-Dev <manas@spotmies.com>
2022-03-21 18:18:41 +0530

    Redesigned Temp. Confirm Order Button

--- lib/view/basket/basket.dart
+++ lib/view/basket/basket.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nandikrushifarmer/reusable_widgets/app_bar.dart';
 import 'package:nandikrushifarmer/reusable_widgets/app_config.dart';
+import 'package:nandikrushifarmer/reusable_widgets/elevated_widget.dart';
 import 'package:nandikrushifarmer/reusable_widgets/text_wid.dart';
 import 'package:nandikrushifarmer/view/basket/confirm_order.dart';
 
@@ -18,14 +19,36 @@ class _BasketState extends State<Basket> {
       appBar: appBarWithTitle(context,
           title: 'Basket', prefix: Icons.shopping_basket_rounded),
       body: Center(
-        child: FlatButton(
-            onPressed: () {
+        child: Padding(
+          padding: EdgeInsets.only(
+              bottom: height(context) * 0.015,
+              left: width(context) * 0.1,
+              right: width(context) * 0.1),
+          child: ElevatedButtonWidget(
+            borderRadius: 8,
+            onClick: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ConfirmOrderScreen()));
+                context,
+                MaterialPageRoute(
+                  builder: ((context) => const ConfirmOrderScreen()),
+                ),
+              );
             },
-            child: Text("ConfirmOrder")),
+            minWidth: width(context) * 0.9,
+            height: height(context) * 0.06,
+            // borderRadius: 16,
+            bgColor: Colors.grey.shade200,
+            textColor: Colors.grey.shade700,
+            textStyle: FontWeight.bold,
+            buttonName: "Confirm Order",
+            textSize: width(context) * 0.04,
+            trailingIcon: Icon(
+              Icons.arrow_forward,
+              color: Colors.grey.shade700,
+              size: width(context) * 0.045,
+            ),
+          ),
+        ),
       ),
     );
   }
